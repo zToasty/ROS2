f6c119861d0c24dda45786f6c1466300d15a9d62
g.karateev <g.karateev@g.nsu.ru>
2025-10-28 14:07:23 +0700

    hell nahn

--- ex02/robot_display.launch.py
+++ ex02/robot_display.launch.py
@@ -1,6 +1,5 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command
 import os
 
 def generate_launch_description():
@@ -8,22 +7,24 @@ def generate_launch_description():
     xacro_file = os.path.join(base_dir, 'ex02/robot.urdf.xacro')
     rviz_config_path = os.path.join(base_dir, 'ex01/pylesos.rviz')
 
-    # Генерация URDF из Xacro
-    robot_desc = Command(['xacro ', xacro_file])
-
     return LaunchDescription([
+        # --- Генерация URDF из Xacro и запуск Robot State Publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_desc}]
+            parameters=[{'robot_description': os.popen(f"xacro {xacro_file}").read()}]
         ),
+
+        # --- GUI для управления суставами
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
         ),
+
+        # --- RViz
         Node(
             package='rviz2',
             executable='rviz2',
